ac181c379efa35832de809453f9c189037e524a2
William Gérald Blondel <contact@williamblondel.fr>
2024-01-10 15:29:29 +0800

    Script to identify dead links

--- scripts/importer/01-create-sqlite-db.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE "urls"
-(
-    "keyword"   varchar PRIMARY KEY NOT NULL DEFAULT '',
-    "url"       text                NOT NULL,
-    "title"     text                         DEFAULT NULL,
-    "timestamp" datetime            NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "ip"        varchar             NOT NULL,
-    "clicks"    int UNSIGNED        NOT NULL
-);
